4d17bfcf9e97150e3fb00b124474a4f93d723fd6
Radamés Roriz <radamesroriz@gmail.com>
2019-08-01 20:47:34 -0300

    feat(models/animal): add cost_per_month rule on create

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -5,18 +5,29 @@ class Animal < ApplicationRecord
 
   enum kind: %i[horse dog parrot alpaca iguana platypus cat swallow]
 
+  LIMIT_PER_PERSON = 1000
+  FORBIDDEN_CHAR = 'a'
+
+  validates :card_number, presence: true
   validate :can_have_swallow?
   validate :can_have_cat?
+  validate :person_can_add_a_animal?, on: :create
 
   def can_have_swallow?
     return unless swallow? && !person.adult?
 
-    errors.add(:status, message: 'person-must-be-adult-to-have-a-swallow')
+    errors.add(:kind, message: 'person-must-be-adult-to-have-a-swallow')
   end
 
   def can_have_cat?
-    return unless cat? && person.name[0].downcase == 'a'
+    return unless cat? && person.name[0].downcase == FORBIDDEN_CHAR
 
-    errors.add(:status, message: 'person-who-has-name-starting-with-a-cant-have-a-cat')
+    errors.add(:kind, message: 'person-who-has-name-starting-with-a-cant-have-a-cat')
+  end
+
+  def person_can_add_a_animal?
+    return unless person.total_cost_by_month > LIMIT_PER_PERSON
+
+    errors.add(:month_cost, message: "person-cant-have-cost-more-than-#{LIMIT_PER_PERSON}")
   end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -6,4 +6,8 @@ class Person < ApplicationRecord
   def adult?
     18.years.ago > birthdate
   end
+
+  def total_cost_by_month
+    animals.sum(:month_cost)
+  end
 end
