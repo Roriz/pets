7db5eb4bdea441dc6b1e977ffa615be630550f99
Radamés Roriz <radamesroriz@gmail.com>
2019-08-01 20:28:49 -0300

    feat(models/animal): add cat rules

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -5,11 +5,18 @@ class Animal < ApplicationRecord
 
   enum kind: %i[horse dog parrot alpaca iguana platypus cat swallow]
 
-  validate :can_have_swallow?, on: :create
+  validate :can_have_swallow?
+  validate :can_have_cat?
 
   def can_have_swallow?
     return unless swallow? && !person.adult?
 
     errors.add(:status, message: 'person-must-be-adult-to-have-a-swallow')
+  end
+
+  def can_have_cat?
+    return unless cat? && person.name[0].downcase == 'a'
+
+    errors.add(:status, message: 'person-who-has-name-starting-with-a-cant-have-a-cat')
   end
 end
